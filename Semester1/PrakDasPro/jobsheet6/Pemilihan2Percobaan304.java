d3088ecca6a52ace43a686ab95f67f8c1f34c475
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-02 21:45:23 +0700

    percobaan 3

---
+++ Semester1/PrakDasPro/jobsheet6/Pemilihan2Percobaan304.java
@@ -0,0 +1,39 @@
+package Semester1.PrakDasPro.jobsheet6;
+
+import java.util.*;
+
+public class Pemilihan2Percobaan304 {
+    public static void main(String[] args) {
+        Scanner input04 = new Scanner(System.in);
+
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+
+        System.out.print("Masukkan kategori : ");
+        kategori = input04.nextLine();
+        System.out.print("Masukkan Besarnya Penghasilan : ");
+        penghasilan = input04.nextInt();
+
+        if (kategori.equalsIgnoreCase("pekerja")) {
+            if (penghasilan <= 2000000) {
+                pajak = 0.1;
+            } else if (penghasilan <= 3000000) {
+                pajak = 0.15;
+            } else 
+                pajak = 0.2;           
+            gajiBersih = (int) (penghasilan - (penghasilan * pajak));
+            System.out.println("Penghasilan Bersih : " + gajiBersih);
+        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+            if (penghasilan <= 2500000) {
+                pajak = 0.15;
+            } else if (penghasilan <= 3500000) {
+                pajak = 0.2;
+            } else 
+                pajak = 0.25;
+            gajiBersih = (int) (penghasilan - (penghasilan * pajak));
+            System.out.println("Penghasilan Bersih : " + gajiBersih);
+        } else 
+            System.out.println("Masukan Kategori Salah ");
+    }
+}
