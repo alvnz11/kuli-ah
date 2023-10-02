95331af5914ade7a6b35beb70fe0ad77b307ced8
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-02 19:56:52 +0700

    jobsheet 6 percobaan 1

---
+++ Semester1/PrakDasPro/jobsheet6/Pemilihan2Percobaan104.java
@@ -0,0 +1,20 @@
+package Semester1.PrakDasPro.jobsheet6;
+
+import java.util.*;
+
+public class Pemilihan2Percobaan104 {
+    public static void main(String[] args) {
+        Scanner input04 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun : ");
+        int tahun = input04.nextInt();
+
+        if (tahun % 4 == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else 
+            System.out.println("Bukan Tahun Kabisat");
+    }
+    
+}
