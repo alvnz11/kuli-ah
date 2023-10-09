1449be8cb18c05634cac21b0ae55d2d854264e57
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-09 18:35:22 +0700

    tugas no 3 percobaan 1

--- Semester1/PrakDasPro/jobsheet7/ForKelipatan04.java
+++ Semester1/PrakDasPro/jobsheet7/ForKelipatan04.java
@@ -7,6 +7,7 @@ public class ForKelipatan04 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+        double rata;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
@@ -17,8 +18,11 @@ public class ForKelipatan04 {
                 counter++;
             }
         }
+        rata = (double) jumlah / counter;
+
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rata);
     }
 
     
