1a797628f8756361e68e5cf3213943b198f46f39
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-23 14:18:51 +0700

    pertanyaan no 1 percobaan 3

--- Semester1/PrakDasPro/jobsheet9/ArrayRataNilai04.java
+++ Semester1/PrakDasPro/jobsheet9/ArrayRataNilai04.java
@@ -5,8 +5,9 @@ public class ArrayRataNilai04 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        double total1 = 0, total2 = 0;
+        double rata2, rataLulus, rataTdkLulus;
+        int counter1 = 0, counter2;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : " );
@@ -14,11 +15,23 @@ public class ArrayRataNilai04 {
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                total1 += nilaiMhs[i];
+                counter1++;
+            } else {
+                total2 += nilaiMhs[i];
+            }
         }
-
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        counter2 = nilaiMhs.length - counter1;
+        rataLulus = total1 / counter1;
+        rataTdkLulus = total2 / counter2;
+        rata2 = (total1 + total2) / nilaiMhs.length;
+        
+        System.out.println("Jumlah mahasiswa yang lulus = " + counter1);
+        System.out.println("Jumlah mahasiswa yang tidak lulus = " + counter2);
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTdkLulus);
+        System.out.println("Rata-rata seluruh nilai = " + rata2);
 
     sc.close();    
     }
