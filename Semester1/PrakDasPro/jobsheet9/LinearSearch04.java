d3bb95cb9d3d8c970e906f78ad3fd2dba481f696
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-25 09:32:09 +0700

    pertanyaan no 3 percobaan 4

--- Semester1/PrakDasPro/jobsheet9/LinearSearch04.java
+++ Semester1/PrakDasPro/jobsheet9/LinearSearch04.java
@@ -1,17 +1,35 @@
+import java.util.*;
+
 public class LinearSearch04 {
     public static void main(String[] args) {
 
-         int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-         int key = 20;
-         int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-         for (int i = 0; i < arrayInt.length; i++) {
+        int key;
+        int hasil = 0;
+        
+        System.out.print("Masukkan jumlah elemen array : ");
+        int jml = sc.nextInt();
+        int [] arrayInt = new int[jml];
+        
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = sc.nextInt();
+         
+        for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
-         }
+                 hasil = i;
+            } 
+        }
 
-         System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        if (arrayInt[hasil] == key) {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }
