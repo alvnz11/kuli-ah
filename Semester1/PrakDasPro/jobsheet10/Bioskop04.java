3ce17f1fc882001ac54825e7082aa2393137cfd1
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-30 18:39:49 +0700

    percobaan 1 no 10

--- Semester1/PrakDasPro/jobsheet10/Bioskop04.java
+++ Semester1/PrakDasPro/jobsheet10/Bioskop04.java
@@ -25,6 +25,12 @@ public class Bioskop04 {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i = 0; i <penonton[2].length; i++ ) {
+            System.out.println(penonton[2][i]);
+        }
+
         // System.out.println(penonton.length);
         // System.out.println(penonton[0].length);
         // System.out.println(penonton[1].length);
