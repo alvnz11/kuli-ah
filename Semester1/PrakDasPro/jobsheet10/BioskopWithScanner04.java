3ac84fddb8f42f464989a64c1c63b197cccd711f
alvnz11 <alvanzasaputra123734@gmail.com>
2023-10-31 17:21:01 +0700

    Percobaan 2 no 6

--- Semester1/PrakDasPro/jobsheet10/BioskopWithScanner04.java
+++ Semester1/PrakDasPro/jobsheet10/BioskopWithScanner04.java
@@ -7,28 +7,64 @@ public class BioskopWithScanner04 {
         Scanner sc = new Scanner(System.in);
 
         int baris, kolom;
-        String nama, next;
-        String [][] penonton = new String[4][2];
+        String nama;
+        boolean loop = true;
+        String [][] penonton = new String[][] {
+            {"***", "***"},
+            {"***", "***"},
+            {"***", "***"},
+            {"***", "***"}
+        };
+        
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.next();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
 
-            penonton[baris-1][kolom-1] = nama;
+        while (loop) {
+            System.out.println();
+            System.out.println("Silahkan pilih menu: ");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Masukkan Pilihan Anda : ");
+            int pilihan = sc.nextInt();
 
-            System.out.print("Input penonton lainnya? (y/n) : ");
-            next = sc.next();
+            switch (pilihan) {
+                case 1:
+                System.out.print("Masukkan nama: ");
+                nama = sc.next();
+                System.out.print("Masukkan baris: ");
+                baris = sc.nextInt();
+                System.out.print("Masukkan kolom: ");
+                kolom = sc.nextInt();
+    
+                
+                if (baris > 0 && baris <= penonton.length && kolom > 0 && kolom <= penonton[0].length) {
+                    if (penonton[baris-1][kolom-1] != "***") {
+                        System.out.println("Kursi Sudah Terisi!, Silahkan Masukkan Baris dan Kolom lainnya!");
+                    } else {
+                        penonton[baris-1][kolom-1] = nama;
+                        System.out.println("Kursi Tersedia!");
+                    }
+                } else {
+                    System.out.println("Kursi tidak tersedia!");
+                }
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case 2:
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join("  ", penonton[i]));
+                }
+                    break;
+
+                case 3:
+                    loop = false;
+                    break;
+                    
+                default:
+                    System.out.println("Input yang anda masukkan salah!");
+                    continue;
             }
         }
 
-    sc.close();
     }
     
 }
